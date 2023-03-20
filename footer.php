8bfb4e2cdc53cbf0ef78390ff4723d21a2615d20
Mufaqar <mufaqar@gmail.com>
2023-03-20 19:42:56 +0500

    Car Theme

--- footer.php
+++ footer.php
@@ -1,13 +1,9 @@
-<?php wp_footer(); ?>
-
-    <script src="<?php bloginfo('template_directory'); ?>/assets/scripts/bootstrap.min.js"></script>
-    <script src="<?php bloginfo('template_directory'); ?>/assets/scripts/jquery.min.js"></script>
-    <script src="<?php bloginfo('template_directory'); ?>/assets/scripts/slick.js"></script>
-
-
-    
-    <script src="<?php bloginfo('template_directory'); ?>/assets/scripts/slick-slider.js"></script>
+        <?php wp_footer(); ?>
+        <script src="<?php bloginfo('template_directory'); ?>/assets/scripts/bootstrap.min.js"></script>
+        <script src="<?php bloginfo('template_directory'); ?>/assets/scripts/jquery.min.js"></script>
+        <script src="<?php bloginfo('template_directory'); ?>/assets/scripts/slick.js"></script>    
+        <script src="<?php bloginfo('template_directory'); ?>/assets/scripts/slick-slider.js"></script>
 
 
-</body>
-</html>
+    </body>
+    </html>
