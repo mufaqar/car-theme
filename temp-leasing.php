6b71de3cdc17ba26401a8ed9d199d8fdaa640fc3
Mufaqar <mufaqar@gmail.com>
2023-03-22 20:30:31 +0500

    Leasing

--- temp-leasing.php
+++ temp-leasing.php
@@ -12,17 +12,17 @@ get_header(); ?>
 
     <section class="leasing-card container mx-auto">
       <div>
-        <img src="./assets/icons/leasing-ico.svg" alt="icon"/>
+        <img src="<?php bloginfo('template_directory'); ?>/assets/icons/leasing-ico.svg" alt="icon"/>
         <h4>No Impact on Your Credit Score</h4>
         <p>Get your credit limit within 15 minutes. Then you're ready to pick your car.</p>
       </div>
       <div>
-        <img src="./assets/icons/leasing-ico.svg" alt="icon"/>
+        <img src="<?php bloginfo('template_directory'); ?>/assets/icons/leasing-ico.svg" alt="icon"/>
         <h4>Only Takes Minutes</h4>
         <p>Just enter a few basic details to instantly see your options</p>
       </div>
       <div>
-        <img src="./assets/icons/leasing-ico.svg" alt="icon"/>
+        <img src="<?php bloginfo('template_directory'); ?>/assets/icons/leasing-ico.svg" alt="icon"/>
         <h4>Personalized Real Rates</h4>
         <p>Know your real monthly payment before visiting the dealer</p>
       </div>
@@ -42,37 +42,37 @@ get_header(); ?>
       <div class="wrapper">
         <div>
           <figure>
-            <img src="./assets/images/subscription-work.png" alt="logo"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/subscription-work.png" alt="logo"/>
           </figure>
           <h6>tabor</h6>
         </div>
         <div>
           <figure>
-            <img src="./assets/images/subscription-work.png" alt="logo"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/subscription-work.png" alt="logo"/>
           </figure>
           <h6>tabor</h6>
         </div>
         <div>
           <figure>
-            <img src="./assets/images/subscription-work.png" alt="logo"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/subscription-work.png" alt="logo"/>
           </figure>
           <h6>tabor</h6>
         </div>
         <div>
           <figure>
-            <img src="./assets/images/subscription-work.png" alt="logo"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/subscription-work.png" alt="logo"/>
           </figure>
           <h6>tabor</h6>
         </div>
         <div>
           <figure>
-            <img src="./assets/images/subscription-work.png" alt="logo"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/subscription-work.png" alt="logo"/>
           </figure>
           <h6>tabor</h6>
         </div>
         <div>
           <figure>
-            <img src="./assets/images/subscription-work.png" alt="logo"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/subscription-work.png" alt="logo"/>
           </figure>
           <h6>tabor</h6>
         </div>
@@ -87,22 +87,22 @@ get_header(); ?>
         <p>With the car subscription, you select a suitable provider and receive a new car or a recently used vehicle. What are the specifics?</p>
         <ul>
           <li>
-            <img src="./assets/icons/yes.svg" alt="icon"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/icons/yes.svg" alt="icon"/>
             <span>You just rent the car. In this way you pay a permanent monthly amount and renounce ownership.</span>
           </li>
           <li>
-            <img src="./assets/icons/yes.svg" alt="icon"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/icons/yes.svg" alt="icon"/>
             <span>With some car subscription providers there are further restrictions, in addition to a minimum age of the driver, for example also the limitation of the annual mileage.</span>
           </li>
           <li>
-            <img src="./assets/icons/yes.svg" alt="icon"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/icons/yes.svg" alt="icon"/>
             <span>If you like driving a vehicle for years, the flexible car subscription is probably not interesting for you.</span>
           </li>
         </ul>
         <a href=""><div>TO THE CAR SUBSCRIPTION OFFERS</div></a>
       </div>
       <div class="_img">
-        <img src="./assets/images/old-car.png" alt="image" >
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/old-car.png" alt="image" >
         <div class="bg-overlay"/>
       </div>
     </section>
@@ -113,26 +113,26 @@ get_header(); ?>
         <p>With the car subscription, you select a suitable provider and receive a new car or a recently used vehicle. What are the specifics?</p>
         <ul>
           <li>
-            <img src="./assets/icons/yes.svg" alt="icon"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/icons/yes.svg" alt="icon"/>
             <span>Depending on the provider, you must be at least 18 or 23 years old.</span>
           </li>
           <li>
-            <img src="./assets/icons/yes.svg" alt="icon"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/icons/yes.svg" alt="icon"/>
             <span>You have a valid EU driver's license in the appropriate vehicle class.</span>
           </li>
           <li>
-            <img src="./assets/icons/yes.svg" alt="icon"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/icons/yes.svg" alt="icon"/>
             <span>Some providers charge a starting fee before the subscription starts.</span>
           </li>
           <li>
-            <img src="./assets/icons/yes.svg" alt="icon"/>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/icons/yes.svg" alt="icon"/>
             <span>Before you subscribe to a car, the provider will check your Schufa entry - so you should have a positive credit rating.</span>
           </li>
         </ul>
         <a href=""><div>TO THE CAR SUBSCRIPTION OFFERS</div></a>
       </div>
       <div class="_img img-left">
-        <img src="./assets/images/old-car.png" alt="image" >
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/old-car.png" alt="image" >
         <div class="bg-overlay ">
       </div>
     </section>
