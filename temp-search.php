1ef9e20c4349652e0edc92c0cb7d724b0681f84d
Mufaqar <mufaqar@gmail.com>
2023-03-29 13:51:18 +0500

    search

---
+++ temp-search.php
@@ -0,0 +1,23 @@
+<?php  /*  Template Name:  Search */
+
+get_header(); ?>
+
+
+    <section class="faq_header">
+      <h2 class="sub-heading">Search</h2>
+      <p>Have you can find frequently asked questions We help you to find the answer</p>
+      
+    </section>
+
+   
+    <section class="container mx-auto faq">
+
+     
+
+    </section>
+
+
+
+  
+
+    <?php get_footer(); ?>
