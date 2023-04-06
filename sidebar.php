a9b0232d7d04540c38138dbf347ac10ec590ca6e
Mufaqar <mufaqar@gmail.com>
2023-04-06 21:59:23 +0500

    updated

--- sidebar.php
+++ sidebar.php
@@ -120,20 +120,3 @@
       </form>
   </aside>
 
-
-  <script type="text/javascript">
-// Using jQuery.
-
-$(function() {
-    $('form').each(function() {
-        $(this).find('input').keypress(function(e) {
-            // Enter pressed?
-            if(e.which == 10 || e.which == 13) {
-                this.form.submit();
-            }
-        });
-
-        $(this).find('input[type=submit]').hide();
-    });
-});
-</script>
