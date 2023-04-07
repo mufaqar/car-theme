1b28aef912b240616e1266a7c2e7188717d17ecb
Mufaqar <mufaqar@gmail.com>
2023-04-07 16:38:29 +0500

    about

--- page.php
+++ page.php
@@ -1,4 +1,10 @@
 <?php get_header(); ?>
+
+<section class="contact-us">
+      <h2 class="sub-heading"><?php the_title()?></h2>
+      <p>One-Stop Platform for Exceptional Automotive Solutions</p>
+   </section>
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <div class="post" id="post-<?php the_ID(); ?>">
         <h1><?php the_title(); ?></h1>
@@ -17,5 +23,5 @@
         </div>
     </div>
 <?php endwhile; endif; ?>
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
