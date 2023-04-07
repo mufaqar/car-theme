82faae2094e6c3f59979644bc3ccc7249c5407a2
Mufaqar <mufaqar@gmail.com>
2023-04-07 16:45:43 +0500

    page

--- page.php
+++ page.php
@@ -6,8 +6,7 @@
    </section>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <div class="post" id="post-<?php the_ID(); ?>">
-        <h1><?php the_title(); ?></h1>
+    <div class="post" id="post-<?php the_ID(); ?>">      
         <div class="entry">
            <?php if ( has_post_thumbnail() ) { ?>
     		    <div class="featured-image">
@@ -15,11 +14,7 @@
     		    </div>
 			<?php } ?>	
 			<?php the_content(); ?>
-			<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-			<div class="tags">
-				<?php the_tags( __('Tags:','text_domain'),'','.'); ?>
-            </div>
-            <div class="clear"></div>
+			
         </div>
     </div>
 <?php endwhile; endif; ?>
