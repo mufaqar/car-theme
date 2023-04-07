dbd49470268c532e4de43e31cb59349ff3b03e85
Mufaqar <mufaqar@gmail.com>
2023-04-07 18:19:50 +0500

    updated

--- temp-search.php
+++ temp-search.php
@@ -21,11 +21,6 @@ if (isset($_REQUEST['transmission'])) {
     $brand = $_REQUEST['transmission'];
 }
 
-
-
-
-
-
 $tax_query = array('relation' => 'AND');
     if (!empty($brand))
     {
@@ -71,35 +66,25 @@ $tax_query = array('relation' => 'AND');
     }
 
   
-
-
-
-
-
 ?>
 
 <section class="search_header container mx-auto">
   <a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_directory'); ?>/assets/icons/logo.svg"
       alt="logo" /></a>
 </section>
-
  <section class="container row mx-auto search">
       <?php get_sidebar()?>
-      <div class="col-md-9 listing">
-         
+      <div class="col-md-9 listing">         
 
-<?php query_posts(array(
-    'post_type' => 'vehicle',
-    'posts_per_page' => -1,
-     'order' => 'desc',
-     'tax_query' => $tax_query	
-    
-)); 
-if (have_posts()) :  while (have_posts()) : the_post();    
-
-          
-          
-          get_template_part( 'template-parts/vehicle', 'card' ); endwhile; wp_reset_query(); else : ?>
+        <?php query_posts(array(
+            'post_type' => 'vehicle',
+            'posts_per_page' => -1,
+            'order' => 'desc',
+            'tax_query' => $tax_query	
+            
+        )); 
+        if (have_posts()) :  while (have_posts()) : the_post();  
+                 get_template_part( 'template-parts/vehicle', 'card' ); endwhile; wp_reset_query(); else : ?>
 			<h2><?php _e('Nothing Found','lbt_translate'); ?></h2>
 	        <?php endif; ?> 
       </div>
