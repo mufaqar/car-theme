b6bca6866df83c2ed3f76ec3dfea0b83aa5f67b3
Mufaqar <mufaqar@gmail.com>
2023-04-09 00:08:52 +0500

    FAQs

--- temp-faqs.php
+++ temp-faqs.php
@@ -3,22 +3,17 @@
 get_header(); ?>
 
 
-    <section class="faq_header">
-      <h2 class="sub-heading">Frequently Asked Questions</h2>
-      <p>Have you can find frequently asked questions We help you to find the answer</p>
-      <div>
-        <input type="text" placeholder="Search your problem or questions">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 25px">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-        </svg>
-      </div>
-    </section>
+<section class="faq_header">
+    <h2 class="sub-heading">Frequently Asked Questions</h2>
+    <p>Have you can find frequently asked questions We help you to find the answer</p>
 
-    <!-- faqs  -->
-    <section class="container mx-auto faq">
+</section>
 
-      <div class="accordion accordion-flush" id="accordionFlushExample">
-        
+<!-- faqs  -->
+<section class="container mx-auto faq">
+
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+
 
 
         <?php query_posts(array(
@@ -31,30 +26,33 @@ get_header(); ?>
               $i= 1 ;
           if (have_posts()) :  while (have_posts()) : the_post(); ?>
 
-                          <div class="accordion-item">
-                            <h2 class="accordion-header" id="<?php echo $i?>">
-                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-<?php echo $i?>" aria-expanded="false" aria-controls="flush-collapse-<?php echo $i?>">
-                              <?php the_title()?>
-                              </button>
-                            </h2>
-                            <div id="flush-collapse-<?php echo $i?>" class="accordion-collapse collapse" aria-labelledby="<?php echo $i?>" data-bs-parent="#accordionFlushExample">
-                              <div class="accordion-body"> <?php the_content()?></div>
-                            </div>
-                          </div>
-              
-
-              <?php $i++; endwhile; wp_reset_query(); else : ?>
-            <h2><?php _e('Nothing Found','lbt_translate'); ?></h2>
-                <?php endif; ?> 
-	
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="<?php echo $i?>">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapse-<?php echo $i?>" aria-expanded="false"
+                    aria-controls="flush-collapse-<?php echo $i?>">
+                    <?php the_title()?>
+                </button>
+            </h2>
+            <div id="flush-collapse-<?php echo $i?>" class="accordion-collapse collapse"
+                aria-labelledby="<?php echo $i?>" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body"> <?php the_content()?></div>
+            </div>
+        </div>
 
 
-      </div>
-
-    </section>
+        <?php $i++; endwhile; wp_reset_query(); else : ?>
+        <h2><?php _e('Nothing Found','lbt_translate'); ?></h2>
+        <?php endif; ?>
 
 
 
-  
+    </div>
 
-    <?php get_footer(); ?>
+</section>
+
+
+
+
+
+<?php get_footer(); ?>
