34baaef8d486790e0d781603ce2c40ded86918c3
Mufaqar <mufaqar@gmail.com>
2023-05-03 12:05:52 +0500

    payemnt

--- inc/payment.php
+++ inc/payment.php
@@ -1,5 +1,16 @@
 <?php
 
 $payment = new Payment ();
+?>
+
+I have checked wireframes and i can convert it to nextjs with taiwlindcss and integration with AWS Server/database and
+implement APIs
+
+Frontend Development : 8-10 days and cost will be 760
+Backend Integration with API and AWS Server : 650
+
+Please share API details so that i can
+
+
 
 ?>
