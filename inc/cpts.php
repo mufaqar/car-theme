cf98ad7aeac84c73f6d71561474b3f497ef17adb
mufaqar <mufaqar@gmail.com>
2023-07-21 16:57:17 +0500

    cpts

--- inc/cpts.php
+++ inc/cpts.php
@@ -401,3 +401,40 @@ function cptui_register_my_cpts_orders() {
 }
 
 add_action( 'init', 'cptui_register_my_cpts_orders' );
+
+function cptui_register_my_taxes_order_type() {
+
+	/**
+	 * Taxonomy: Types.
+	 */
+
+	$labels = [
+		"name" => esc_html__( "Types", "custom-post-type-ui" ),
+		"singular_name" => esc_html__( "Type", "custom-post-type-ui" ),
+	];
+
+	
+	$args = [
+		"label" => esc_html__( "Types", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => false,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'order_type', 'with_front' => true, ],
+		"show_admin_column" => true,
+		"show_in_rest" => true,
+		"show_tagcloud" => false,
+		"rest_base" => "order_type",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"rest_namespace" => "wp/v2",
+		"show_in_quick_edit" => true,
+		"sort" => true,
+		"show_in_graphql" => true,
+	];
+	register_taxonomy( "order_type", [ "orders" ], $args );
+}
+add_action( 'init', 'cptui_register_my_taxes_order_type' );
