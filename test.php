1623e66fa1fb1d59a14c72366dd8c2cb494b5a49
mufaqar <mufaqar@gmail.com>
2023-07-27 13:20:34 +0500

    mail

--- test.php
+++ test.php
@@ -14,16 +14,9 @@ get_header(); ?>
 		$headers[] = 'From: mufaqar@gmail.com" . "\r\n';
 		$headers[] = 'Cc: mufaqar2@gmail.com';	
 		$headers[] = "Content-Type: text/html; charset=UTF-8\r\n";
-		wp_mail( $admin, $subject, $body, $headers );
+		mail( $admin, $subject, $body, $headers );
         echo "Email sent Sucessfully 01";
 
-        $msg = "First line of text\nSecond line of text";
-
-        // use wordwrap() if lines are longer than 70 characters
-        $msg = wordwrap($msg,70);
-
-        // send email
-        mail($admin,"My subject",$msg);
 	}
 
     
