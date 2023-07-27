d27d3873c926bd55850795370f2da2a942a28248
mufaqar <mufaqar@gmail.com>
2023-07-27 14:41:49 +0500

    updated

--- inc/emails.php
+++ inc/emails.php
@@ -3,14 +3,14 @@
 
 	// Send email to Agent
 	function sendmail_agent($agent_email,$post_id) {		
-		$admin = 'mufaqar@gmail.com';
+		$admin = 'lp@001cars.com';
 		$subject = '001 Cars | Vehicle Listing Notification ';
 		$body  = "<h1>Congrgralations your add sucessfully submited</h1> ";
 		$body  .= "<p><img src='https://001cars.com/wp-content/uploads/2023/07/logo.png' width='320px'></img></p><hr/> ";
 		$body  .= "<p><strong> You Vehicle Listed Sucessfully once admin approve it it wil be display on the website. </strong>  </p> ";
 		$headers = "MIME-Version: 1.0" . "\r\n";
 		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-		$headers .= "From: info@001cars.com" . "\r\n" . "CC: $agent_email";			
+		$headers .= "From: info@001cars.com" . "\r\n" ;			
 		mail( $admin, $subject, $body, $headers );
 		
 	}
@@ -18,7 +18,7 @@
 	// Send Admint Notification 
 
 	function sendmail_admin($agent_email,$post_id) {	
-		$admin = 'mufaqar@gmail.com';
+		$admin = 'lp@001cars.com,mufaqar@gmail.com';
 		$subject = '001 Cars | Admin Notification ';
 		$body  = "<h1>New Ads added on website , Please approve it</h1> ";
 		$body  .= "<p><img src='https://001cars.com/wp-content/uploads/2023/07/logo.png' width='320px'></img></p><hr/> ";
