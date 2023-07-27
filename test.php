3aa8e9c7eb7b05624658f198cfce380a31b4c6af
mufaqar <mufaqar@gmail.com>
2023-07-27 13:25:13 +0500

    email

--- test.php
+++ test.php
@@ -12,7 +12,6 @@ get_header(); ?>
 		$body  .= "<p><img src='https://001cars.com/wp-content/themes/car-theme/assets/icons/logo.svg' width='320px'></img></p><hr/> ";
 		$body  .= "<p><strong> Email Address: </strong> $user_email </p> ";
 		$headers = "From: info@001cars.com" . "\r\n" . "CC: mufaqar@gmail.com";
-
         $headers .= "MIME-Version: 1.0" . "\r\n";
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
         
