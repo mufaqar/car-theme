b9d62019cca763bc6776bb3501fa3d5c96d657eb
mufaqar <mufaqar@gmail.com>
2023-08-21 14:24:26 +0500

    Gallery

--- template-parts/home-gallery.php
+++ template-parts/home-gallery.php
@@ -1,40 +1,72 @@
 <section class="gallery_sec">
 
 
-<div class="container mx-auto listing_page_wrapper ">
+    <div class="container mx-auto listing_page_wrapper ">
 
-    <div class="slider-card-wrapper">
-        <div class="slider-card">
-            <a href="#">
-                <img width="100%" height="auto"
-                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery1.jpg"
-                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
-                    loading="lazy" /></a>
+        <div class="slider-card-wrapper">
+            <div class="slider-card">
+                <a href="#">
+                    <img width="100%" height="auto"
+                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery1.jpg"
+                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
+                        decoding="async" loading="lazy" /></a>
 
+            </div>
         </div>
-    </div>
 
-    <div class="slider-card-wrapper">
-        <div class="slider-card">
-        <a href="#">
-                <img width="100%" height="auto"
-                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery2.jpg"
-                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
-                    loading="lazy" /></a>
+        <div class="slider-card-wrapper">
+            <div class="slider-card">
+                <a href="#">
+                    <img width="100%" height="auto"
+                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery2.jpg"
+                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
+                        decoding="async" loading="lazy" /></a>
 
+            </div>
         </div>
-    </div>
 
-    <div class="slider-card-wrapper">
-        <div class="slider-card">
-        <a href="#">
-                <img width="100%" height="auto"
-                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery3.jpg"
-                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
-                    loading="lazy" /></a>
+        <div class="slider-card-wrapper">
+            <div class="slider-card">
+                <a href="#">
+                    <img width="100%" height="auto"
+                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery3.jpg"
+                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
+                        decoding="async" loading="lazy" /></a>
 
+            </div>
         </div>
+
+        <div class="slider-card-wrapper">
+            <div class="slider-card">
+                <a href="#">
+                    <img width="100%" height="auto"
+                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery4.jpg"
+                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
+                        decoding="async" loading="lazy" /></a>
+
+            </div>
+        </div>
+        <div class="slider-card-wrapper">
+            <div class="slider-card">
+                <a href="#">
+                    <img width="100%" height="auto"
+                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery5.jpg"
+                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
+                        decoding="async" loading="lazy" /></a>
+
+            </div>
+        </div>
+        <div class="slider-card-wrapper">
+            <div class="slider-card">
+                <a href="#">
+                    <img width="100%" height="auto"
+                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery6.jpg"
+                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
+                        decoding="async" loading="lazy" /></a>
+
+            </div>
+        </div>
+
+
     </div>
-
-
-</div></section >
+</section>
