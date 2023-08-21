b8f630e24ada7c688d71cf457c84d57e70cde869
mufaqar <mufaqar@gmail.com>
2023-08-21 14:30:26 +0500

    link

--- template-parts/home-gallery.php
+++ template-parts/home-gallery.php
@@ -5,94 +5,94 @@
 
         <div class="slider-card-wrapper">
             <div class="slider-card">
-                <a href="#">
-                    <img width="100%" height="auto"
-                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery1.jpg"
-                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
-                        decoding="async" loading="lazy" /></a>
+
+                <img width="100%" height="auto"
+                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery1.jpg"
+                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
+                    loading="lazy" />
 
             </div>
         </div>
 
         <div class="slider-card-wrapper">
             <div class="slider-card">
-                <a href="#">
-                    <img width="100%" height="auto"
-                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery2.jpg"
-                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
-                        decoding="async" loading="lazy" /></a>
+
+                <img width="100%" height="auto"
+                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery2.jpg"
+                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
+                    loading="lazy" />
 
             </div>
         </div>
 
         <div class="slider-card-wrapper">
             <div class="slider-card">
-                <a href="#">
-                    <img width="100%" height="auto"
-                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery3.jpg"
-                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
-                        decoding="async" loading="lazy" /></a>
+
+                <img width="100%" height="auto"
+                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery3.jpg"
+                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
+                    loading="lazy" />
 
             </div>
         </div>
 
         <div class="slider-card-wrapper">
             <div class="slider-card">
-                <a href="#">
-                    <img width="100%" height="auto"
-                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery4.jpg"
-                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
-                        decoding="async" loading="lazy" /></a>
+
+                <img width="100%" height="auto"
+                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery4.jpg"
+                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
+                    loading="lazy" />
 
             </div>
         </div>
         <div class="slider-card-wrapper">
             <div class="slider-card">
-                <a href="#">
-                    <img width="100%" height="auto"
-                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery5.jpg"
-                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
-                        decoding="async" loading="lazy" /></a>
+
+                <img width="100%" height="auto"
+                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery5.jpg"
+                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
+                    loading="lazy" />
 
             </div>
         </div>
         <div class="slider-card-wrapper">
             <div class="slider-card">
-                <a href="#">
-                    <img width="100%" height="auto"
-                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery6.jpg"
-                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
-                        decoding="async" loading="lazy" /></a>
+
+                <img width="100%" height="auto"
+                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery6.jpg"
+                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
+                    loading="lazy" />
 
             </div>
         </div>
         <div class="slider-card-wrapper">
             <div class="slider-card">
-                <a href="#">
-                    <img width="100%" height="auto"
-                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery7.jpg"
-                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
-                        decoding="async" loading="lazy" /></a>
+
+                <img width="100%" height="auto"
+                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery7.jpg"
+                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
+                    loading="lazy" />
 
             </div>
         </div>
         <div class="slider-card-wrapper">
             <div class="slider-card">
-                <a href="#">
-                    <img width="100%" height="auto"
-                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery8.jpg"
-                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
-                        decoding="async" loading="lazy" /></a>
+
+                <img width="100%" height="auto"
+                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery8.jpg"
+                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
+                    loading="lazy" />
 
             </div>
         </div>
         <div class="slider-card-wrapper">
             <div class="slider-card">
-                <a href="#">
-                    <img width="100%" height="auto"
-                        src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery9.jpg"
-                        class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt=""
-                        decoding="async" loading="lazy" /></a>
+
+                <img width="100%" height="auto"
+                    src="<?php bloginfo('template_directory'); ?>/assets/images/car-gallery9.jpg"
+                    class="attachment-vehicle-thumbnail size-vehicle-thumbnail wp-post-image" alt="" decoding="async"
+                    loading="lazy" />
 
             </div>
         </div>
