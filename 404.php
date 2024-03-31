df5744fa6f27ddf1c19d806429c74d0fbfa870b1
Mufaqar Islam <mufaqar@gmail.com>
2024-03-31 17:49:07 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,4 +1,4 @@
 <?php get_header(); ?>
-	<h1><?php _e('404 Error page, The page you are looking for does not exist','car-theme'); ?></h1> 
+<h1><?php _e('404 Error page, The page you are looking for does not exist','car-theme'); ?></h1> 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
